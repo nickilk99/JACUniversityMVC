be789c91ee585be79cb5f7570d0b6ca1faf25e74
Nick <nickilkic@gmail.com>
2020-05-28 17:18:55 -0400

    Added models

---
+++ JACUniversityMVC/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JACUniverstityMVC.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        [NotMapped]
+        public string FullName => this.FirstName + " " + this.LastName;
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
