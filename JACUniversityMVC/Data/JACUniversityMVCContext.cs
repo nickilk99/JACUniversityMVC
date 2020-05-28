464eb8127e055c41b0603f43e3545f51a900493c
Nick <nickilkic@gmail.com>
2020-05-28 17:32:42 -0400

    Merge

---
+++ JACUniversityMVC/Data/JACUniversityMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using JACUniverstityMVC.Models;
+
+namespace JACUniversityMVC.Data
+{
+    public class JACUniversityMVCContext : DbContext
+    {
+        public JACUniversityMVCContext (DbContextOptions<JACUniversityMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JACUniverstityMVC.Models.Student> Student { get; set; }
+    }
+}
--- JACUniversityMVC/Startup.cs
+++ JACUniversityMVC/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using JACUniversityMVC.Data;
 
 namespace JACUniversityMVC
 {
@@ -32,6 +34,9 @@ namespace JACUniversityMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<JACUniversityMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("JACUniversityMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
